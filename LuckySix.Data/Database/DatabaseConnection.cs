252f40ff5cdcb9dcd58412d3e57b120483fd8ecb
tonikopilas <tonykopilas11@gmail.com>
2021-04-24 11:30:57 +0200

    com on

---
+++ LuckySix.Data/Database/DatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckySix.Data.Database
+{
+    public class DatabaseConnection
+    {
+        public string connectionString { get; set; }
+
+        public DatabaseConnection()
+        {
+            connectionString = "Server=uk.sql01.yourwebservers.com,1786;Database=bingo;User Id=bingo;Password=bingo;";
+        }
+    }
+}
